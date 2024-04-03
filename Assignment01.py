83cf267890b89029573cd95c7112244d2ff9c007
Paolo Gomez <peweegardner248@gmail.com>
2024-04-02 23:05:19 -0700

    Initial commit

---
+++ Assignment01.py
@@ -0,0 +1,26 @@
+'''
+1. 
+
+O(1) - Constant Time
+Turning on a light switch. Doesnt matter how big the room is or how many switches; flipping the switch itself takes the same amount of time.
+
+O(log n) - Logarithmic Time
+Guessing a number between 1 and 100. If you know that it is higher or lower after each guess, you can narrow down the possibilities by half each time. 
+More efficiency than guessing one at a time. 
+
+O(n) - Linear Time
+Counting the number of students in a classroom by name one by one then adding them up. Time increases linearly in accordance to the number of students.
+
+O(n log n) - Linearithmic Time
+Sorting a collection of marbles by color. Divide marbles into groups based on color, sort each group, then put back together.
+As you add more marbles, it may take longer to sort, but easier than comparing each marble to every other one. 
+
+O(n^2) - Quadratic Time
+Counting the total number of stickers on a sheet where each row and column has one sticker. If 3 x 3, you need 3 stickers for first row, 3 for second row, and 3 for third row, total 9 stickers. 
+The total number of stickers counted grows quadratically with size of sheet. 
+
+
+'''
+
+# 2.
+
